f262a0b8926606bc8b2cfb60a113efc504eadbd7
Frederik Riewerts <frederik.riewerts@gmail.com>
2019-09-13 08:23:44 +0200

    added renditions and multi format

--- index.js
+++ index.js
@@ -7,14 +7,48 @@ const renderImages = async () => {
   return await Promise.all(files.map(async (file) => {
     const curFilename = basename(file, '.jpg');
     const curImg = await sharp(file).toBuffer();
-    await sharp(curImg)
-      .resize(220, 165)
+
+    // generte sizes
+    const desk = await sharp(curImg).resize(1224).toBuffer();
+    const tab = await sharp(curImg).resize(220, 165).toBuffer();
+    const mob = await sharp(curImg).resize(200, 150).toBuffer();
+
+    // Desktop images
+    await sharp(desk)
+      .webp({reductionEffort: 6, quality: 70})
+      .toFile(`target/${curFilename}.webp`);
+
+    await sharp(desk)
+      .jpeg({
+        quality: 90,
+        chromaSubsampling: '4:4:4'
+      })
+      .toFile(`target/${curFilename}.jpeg`);
+
+    // tablet images
+    await sharp(tab)
       .webp({reductionEffort: 6, quality: 70})
       .toFile(`target/${curFilename}__220x165.webp`);
-    await sharp(curImg)
-      .resize(200, 150)
+
+    await sharp(tab)
+      .jpeg({
+        quality: 90,
+        chromaSubsampling: '4:4:4'
+      })
+      .toFile(`target/${curFilename}__220x165.jpeg`);
+
+    // mobile images
+    await sharp(mob)
       .webp({reductionEffort: 6, quality: 70})
       .toFile(`target/${curFilename}__200x150.webp`);
+
+    await sharp(mob)
+      .jpeg({
+        quality: 90,
+        chromaSubsampling: '4:4:4'
+      })
+      .toFile(`target/${curFilename}__200x150.jpeg`);   
+
   }));
 };
 console.time();
